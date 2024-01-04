170b345e93381a13f93e8471e4770b6104848c6f
shefat26 <shefatmoral@gmail.com>
2024-01-04 11:26:34 -0500

    new

--- arr.js
+++ arr.js
@@ -1,4 +1,66 @@
 
 let a = [1,2,3,4]
 
-console.log(a.length)
+let newArr = a ;
+
+console.log(typeof newArr);
+console.log(newArr);
+
+
+
+let obj = {
+
+    name: "sifat"
+}
+
+console.log(obj.age);
+
+// console.log("sifat")
+
+
+// primitive data type
+
+// number, string, boolean, Null, Undefined, symbole
+
+
+// referrance data type
+// Object, function, Array, Date, and other type of object.
+
+
+
+// let , Var , Const
+
+
+//declare with value assigning
+var b = 1
+
+// re-assigning value
+ b = "name"
+ 
+ //re-declare
+var b = ""
+
+
+
+//We can't re-declare Let keyword in javascript
+// We can re-assigne Let keyword in javavscript
+
+// just declare
+let c 
+
+
+
+
+// Assigning value to "C"
+c = "work"
+
+// re-Assigning value to "C"
+c = 23
+
+//re-declare "c", we can't re-declare "c" with Let, var and Const Keyword . 
+// let c = "name"  
+
+
+
+
+
