d460677d77c55dd925eac0192da73d84e383a3ad
shefat26 <shefatmoral@gmail.com>
2024-01-08 18:52:49 -0500

    array  practice

---
+++ newArr.js
@@ -0,0 +1,32 @@
+
+// 1) Store Multiple Values with Arrays
+
+
+var ourArray = ["John", 23];
+
+console.log(ourArray);
+
+
+
+
+
+// 2) Nested Array / Multidimentional array
+
+
+var ourArray = [["the universe", 42, 55], "ALACE",235, ["everything", 1001]]
+
+console.log(ourArray[0][2])
+console.log(ourArray[1][3])
+console.log(ourArray[3][0])
+console.log(ourArray[3][1])
+console.log(ourArray[2])
+
+
+
+
+
+
+
+
+
+
