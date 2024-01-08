3f1ce8e7c19b3d6528abf6312bd9b4159d5969fb
shefat26 <shefatmoral@gmail.com>
2024-01-08 18:53:20 -0500

    array  practice

--- newArr.js
+++ newArr.js
@@ -24,6 +24,71 @@ console.log(ourArray[2])
 
 
 
+// 3) Access array data with index
+
+
+var anArray = [50, 60, 70, 8]
+
+console.log(anArray[1]);
+console.log(anArray[0]);
+console.log(anArray[3]);
+
+
+
+
+
+
+// Modify Array Data with Index
+
+
+var myArray = [20,64,99,33]
+console.log(myArray);
+
+myArray[0] = 85;
+console.log(myArray)
+
+
+console.log("\n", "\n###########################\n")
+
+
+
+
+
+
+// 4) Manipulate Arrays with push()
+
+
+
+var anArray = ["John", "jay", "cat"];
+console.log(anArray);
+
+
+anArray.push("Ron", "Joy");
+console.log(anArray);
+
+
+var anArray = [["James", 23], ["Dog", 3]];
+console.log(anArray)
+
+anArray.push(["bird",1])
+console.log(anArray, "\n\n");
+
+
+
+
+
+
+
+// Manipulate Arrays with pop()
+
+
+var newArr = [1,2,3,4]
+console.log(newArr)
+
+var removedArr = newArr.pop()
+
+console.log(newArr);
+console.log(removedArr,"\n");
 
 
 
