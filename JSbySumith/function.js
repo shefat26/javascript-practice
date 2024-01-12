8193dce406156972de0e38acdcfedebd25d95cb3
shefat26 <shefatmoral@gmail.com>
2024-01-12 18:38:03 -0500

    function practice

--- JSbySumith/function.js
+++ JSbySumith/function.js
@@ -5,3 +5,52 @@ function myFunction(a, b) {
 let x = myFunction(3, 6);
 console.log(x);
 
+
+// Function
+function toCelcious(fahrenheight) {
+   return (5 / 9) * (fahrenheight - 32);
+ }
+ 
+ console.log(toCelcious(77));
+ 
+ //
+ 
+ function myCar() {
+   let carName = "Volvo";
+ 
+   console.log(carName);
+ }
+ 
+ myCar();
+ 
+ ///
+ 
+ let carName = "BMW";
+ 
+ console.log(carName);
+ 
+ carName = "Toyata";
+ console.log(carName);
+ 
+ //
+ 
+ function logger(param, param2) {
+   console.log(param, param2);
+ }
+ 
+ let a = ["car", "Bus"];
+ let b = 10;
+ 
+ logger(a, b);
+ logger(33, 45);
+ logger({ key: "lock" }, "object");
+ 
+ let person = {
+   job: "IT",
+   age: 27,
+   name: "John",
+   cars: a,
+ };
+ 
+ const {job, age, name,cars}= person;
+ console.log(job);
