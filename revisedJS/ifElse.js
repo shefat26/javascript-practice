8f50dba3b345990d8168be69fab3587d36d48ea7
shefat26 <shefatmoral@gmail.com>
2024-11-25 19:23:04 -0500

    Ternary operator practice

--- revisedJS/ifElse.js
+++ revisedJS/ifElse.js
@@ -1,6 +1,8 @@
 
 // "if" "else"  statement 
 
+// Ternary Operator
+
 
 function checkAge(age){
 
@@ -116,6 +118,95 @@ else{
     console.log("None of them")
 
 }
+
+
+console.log("\n###### Ternary Operator #######\n")
+
+
+
+
+// "Ternary" operator 
+// Ternary operator is replace of "if"  "else" statement, 
+// "Ternary" operator can have multiple "Ternary" operatore inside the operator
+// "Ternary" operator can be chaned together.
+
+let i = 10;
+let j = 12;
+
+let result = i>j ? console.log("i is greater") : console.log("j is greater");
+
+console.log(result);
+
+
+
+//
+
+let product = "phone";
+
+/* so here we are saying if the "product" holds any value it will print 
+ the first part of the "ternary" operator, else it will print the 
+ second part.
+ */
+
+ //           =condition ?  "true"     :      "false"
+let responce = product? "it's a phone" : "it's a Telivision ";
+
+console.log(responce);
+
+
+
+
+
+/*/ Chaining or multiple "Ternary" operator inside one "Ternary" operator, 
+ we seperated multiple "Ternary" operator by placing " : " after every
+ first "true" statement. 
+*/
+
+let testScore = 50;
+
+let myGrade = testScore > 89? `Grade "A"`:testScore > 79? `Grade B ` 
+             :testScore > 69? ` Grade  C ` 
+             :testScore > 59? ` Grade  D ` 
+             : ` F ` ;
+
+ console.log(myGrade);
+
+
+
+
+
+ // Multiple "Ternary" operator
+
+
+ let firstCheck = false;
+ let secondCheck = false;
+
+ // here we wrote two "Ternary" operator, inside one "Ternary" operator
+ access=firstCheck?"Access denied": secondCheck?"Access denied":"Access granted"
+
+ console.log(access);
+
+
+
+
+
+
+ // "Rock", "Paper" and "Scissors" game for "Ternary" operator
+
+
+ let playerOne = "rock";
+ let playerTwo= "rock"
+
+ // variable =      "condition"     ?      "true"    :      "false"
+ let result1 = playerOne===playerTwo?"player matched":"different player"
+
+ console.log(result1);
+
+
+
+
+
+
 
 
 
